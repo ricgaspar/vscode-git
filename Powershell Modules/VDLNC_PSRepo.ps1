be6ea17ff665bcc6f67809b348000455392d3ee3
Jussen <ADMMJ90624@nedcar.nl>
2018-02-15 09:25:32 +0100

    Commit

---
+++ Powershell Modules/VDLNC_PSRepo.ps1
@@ -0,0 +1,10 @@
+# $Path = '\\s007.nedcar.nl\PSRepository$'
+# $repo = @{
+#    Name = 'VNBPSRepo'
+#    SourceLocation = $Path
+#    PublishLocation = $Path
+#    InstallationPolicy = 'Trusted'
+# }
+# Register-PSRepository @repo -Verbose
+
+# Publish-Module -Name VNB_PSLib -RequiredVersion 2.2.0.3 -Repository VNBPSRepo -Verbose
