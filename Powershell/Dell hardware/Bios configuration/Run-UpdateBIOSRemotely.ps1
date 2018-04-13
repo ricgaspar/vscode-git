f78dbe0c741170095de64d982a45494307ed967f
Jussen <ADMMJ90624@nedcar.nl>
2018-04-13 12:55:15 +0200

    Update

---
+++ Powershell/Dell hardware/Bios configuration/Run-UpdateBIOSRemotely.ps1
@@ -0,0 +1,195 @@
+# ------------------------------------------------------------------------------
+<#
+.SYNOPSIS
+
+
+.CREATED_BY
+	Marcel Jussen
+
+.CREATE_DATE
+	11-09-2017
+
+.CHANGE_DATE
+	11-09-2017
+
+.DESCRIPTION
+
+
+#>
+# ------------------------------------------------------------------------------
+#-requires 5.0
+
+# ---------------------------------------------------------
+Import-Module VNB_PSLib -Force -ErrorAction Stop
+# ---------------------------------------------------------
+
+$Global:DEBUG = $false
+Function Get-RemoteComputersFromAD {
+    param (
+        [string]$ADSearchFilter = '(objectCategory=Computer)',
+        [string]$OUPath
+    )
+
+    begin {
+        try {
+            $colResults = $null
+            $objOU = New-Object System.DirectoryServices.DirectoryEntry($OUPath)
+            $objSearcher = New-Object System.DirectoryServices.DirectorySearcher
+        }
+        catch { }
+    }
+    process {
+        try {
+            $objSearcher.SearchRoot = $objOU
+            $objSearcher.PageSize = 5000
+            $objSearcher.Filter = $ADSearchFilter
+            $colResults = $objSearcher.FindAll()
+        }
+        catch { }
+        return $colResults
+    }
+}
+
+# ---------------------------------------------------------
+Clear-Host
+
+# Initialise log file
+$LogPath = 'C:\Logboek\RUN-UpdateBIOSRemotely.log'
+[void](Init-Log -LogFileName $LogPath $False -alternate_location $True)
+
+# Record start of script.
+$BaseStart = Get-Date
+Echo-Log ("=" * 80)
+Echo-Log "Start CRC maintenance run."
+Echo-Log ("=" * 80)
+
+#
+# Search Active Directory for computers
+$ADOU = 'LDAP://OU=FACTORY,DC=nedcar,DC=nl'
+Echo-Log "Collecting computers from $ADOU"
+$DSComputers = Get-RemoteComputersFromAD -OUPath $ADOU | Where-Object { $_.properties.dnshostname -eq 'vdlnc00429.nedcar.nl'}
+
+# $ADOU = 'LDAP://OU=CLIENTS,DC=nedcar,DC=nl'
+# Echo-Log "Collecting computers from $ADOU"
+# $DSComputers = Get-RemoteComputersFromAD -OUPath $ADOU | Where-Object { $_.properties.dnshostname -eq 'vdlnc01800.nedcar.nl'}
+
+if ($DSComputers -eq $null) {
+    Echo-Log "ERROR: No computers collected."
+}
+else {
+    $JobTimer = @{}
+
+    $count = 0
+    $startedcount = 0
+    $DSComputers | ForEach-Object {
+        $CompName = [System.String]$_.properties.dnshostname
+
+        # Format the job name per computer
+        $JobHeader = "CancelShutDownRestart_"
+        $JobName = $JobHeader + $CompName
+
+        $CurrTime = Get-Date
+        $JobTimer.add("$CompName", $CurrTime)
+        $startedcount++
+
+        $LogText = "($startedcount) Trying to start job for $CompName"
+        Echo-Log $LogText
+
+        # Start job
+        Start-Job -Name $JobName -ArgumentList $CompName -ScriptBlock {
+            # This is the script block actually executed for each computer
+            $Error.Clear()
+            $CompName = $args[0]
+
+            Invoke-Command -ComputerName $CompName -FilePath "C:\VSCode\vscode-git\Powershell\Dell hardware\Bios configuration\Set-WakeOnLan.ps1"
+
+        } | Out-Null
+
+        # Check on running jobs
+        Receive-Job -Name "$JobHeader*" | ForEach-Object {
+            $count++
+            $Output = $_
+            foreach ($strOutput in $Output) {
+                Echo-Log $strOutput
+            }
+        }
+
+        # Check if number of running jobs does not exceed 20
+        do {
+            Echo-Log "Check running jobs status."
+            $RunningJobs = 0
+            $IgnoredJobs = 0
+            Get-Job | where-object {$_.Name -like "$JobHeader*" -and $_.State -eq "Running"} | ForEach-Object {
+                $JobID = $_.ID
+                if ($SkippedJobs -inotcontains "$JobID") {
+                    $RunningJobs++
+                    $CurrTime = Get-Date
+                    $JobCompName = $_.Name
+                    $JobCompName = $JobCompName.replace($JobHeader, "")
+                    $StartTime = $JobTimer["$JobCompName"]
+                    $CompareTime = $CurrTime - $StartTime
+                    if ($CompareTime.Minutes -gt 2 -and $IgnoredJobs -eq 0) {
+                        $SkippedJobs += @("$JobID")
+                        $IgnoredJobs++
+                        $NumUnsuccess++
+                    }
+                }
+            }
+            # Wait for any jobs to complete
+            if ($RunningJobs -gt 20) {
+                Echo-Log "Waiting for jobs to complete..."
+                Start-Sleep 1
+            }
+        } while ($RunningJobs -gt 20)
+    }
+
+    # Check for jobs to complete
+    do {
+        Echo-Log "Check job completion."
+        $RunningJobs = 0
+        $IgnoredJobs = 0
+        get-job | where-object {$_.Name -like "$JobHeader*" -and $_.State -eq "Running"} | ForEach-Object {
+            $JobID = $_.ID
+            if ($SkippedJobs -inotcontains "$JobID") {
+                $RunningJobs++
+                $CurrTime = Get-Date
+                $JobCompName = $_.Name
+                $JobCompName = $JobCompName.replace($JobHeader, "")
+                $StartTime = $JobTimer["$JobCompName"]
+                $CompareTime = $CurrTime - $StartTime
+                if ($CompareTime.Minutes -gt 2 -and $IgnoredJobs -eq 0) {
+                    $SkippedJobs += @($_.ID)
+                    $IgnoredJobs++
+                    $NumUnsuccess++
+                }
+            }
+        }
+        if ($RunningJobs -gt 0) {
+            Echo-Log "Waiting for jobs to complete..."
+            Start-Sleep 1
+        }
+    } while ($RunningJobs -gt 0)
+
+    # Check output of all jobs completed.
+    Receive-Job -Name "$JobHeader*" | ForEach-Object {
+        $Output = $_
+        foreach ($strOutput in $Output) {
+            echo-log $strOutput
+        }
+    }
+
+    Echo-Log "Removing jobs."
+    Remove-Job -Name "$JobHeader*" | ForEach-Object {
+    }
+}
+
+$min = New-TimeSpan -Start ($BaseStart) -End (Get-Date)
+Echo-Log ("Run time: $min)")
+Echo-Log ("=" * 80)
+
+# =========================================================
+Close-LogSystem
+
+# =========================================================
+# Thats all folks..
+# =========================================================
--- Powershell/Dell hardware/Bios configuration/Set-WakeOnLan.ps1
+++ Powershell/Dell hardware/Bios configuration/Set-WakeOnLan.ps1
@@ -1,45 +1,151 @@
-Import-Module -Name DellBIOSProvider
-$password = 'kleinevogel'
+# =========================================================
+# VDL Nedcar - Information Systems
+#
+# .SYNOPSIS
+# 	Update Dell BIOS parameters for Wake On LAN feature
+#
+# .CREATED_BY
+# 	Marcel Jussen
+#
+# .CHANGE_DATE
+# 	13-04-2018
+#
+# .DESCRIPTION
+#	Updates the WakeOnLAN and DeepSleepCtrl parameters to enable Wake On LAN
+#
+# =========================================================
+#Requires -version 5.0
+
+Set-StrictMode -Version Latest
+
+# Update Dell BIOS to accept Wake-On-LAN
+Write-Output ("-" * 80)
+Write-Output "[$env:COMPUTERNAME] Start of script."
+
+# New-Item -Path "$Env:ProgramData\VDL Nedcar\Logboek\BIOS" -ItemType Directory -Force -ErrorAction SilentlyContinue | Out-Null
+# Start-Transcript -Path "$Env:ProgramData\VDL Nedcar\Logboek\BIOS\VNB-BIOS-Update-WakeOnLan.log" -Force
+# Write-Output "[$env:COMPUTERNAME] Start of transscript logging."
+
+#
+# WARNING
+# Dell Optiplex 7040 and Latitude E5570 share a common BIOS model which has a critical bug when version < 1.5.0
+#
+$BIOSver = Get-CimInstance -ClassName Win32_BIOS -ErrorAction Stop | Select-Object SMBIOSBIOSVersion
+$Make = Get-CimInstance -ClassName Win32_ComputerSystem -ErrorAction Stop | Select-Object Manufacturer, Model
+$AcceptedBIOSMajor = '1'
+$AcceptedBIOSMinor = '5'
+$AcceptedBIOS = $AcceptedBIOSMajor + '.' + $AcceptedBIOSMinor
+if ($Make.Manufacturer -eq 'Dell Inc.') {
+    Write-Output "[$env:COMPUTERNAME] Computername: $env:COMPUTERNAME"
+    Write-Output "[$env:COMPUTERNAME] Make: $($Make.Manufacturer)"
+    if (($Make.Model -eq 'Optiplex 7040') -or ($Make.Model -eq 'Latitude E5570')) {
+        Write-Output "[$env:COMPUTERNAME] Model (*): $($Make.Model)"
+        $VerArray = $($Biosver.SMBIOSBIOSVersion).split('.')
+        Write-Output "[$env:COMPUTERNAME] Major: $($VerArray[0])"
+        Write-Output "[$env:COMPUTERNAME] Minor: $($VerArray[1])"
+
+        if ( ($($VerArray[0]) -ge $AcceptedBIOSMajor ) -and ( $($VerArray[1]) -ge $AcceptedBIOSMinor) ) {
+            Write-Output "[$env:COMPUTERNAME] BIOS: $($BIOSver.SMBIOSBIOSVersion)"
+            Write-Output "[$env:COMPUTERNAME] A valid BIOS version was found."
+        }
+        else {
+            Write-Output "[$env:COMPUTERNAME] BIOS (*): $($BIOSver.SMBIOSBIOSVersion)"
+            Write-Output "[$env:COMPUTERNAME] ERROR: BIOS version is not valid. Cannot update this BIOS."
+            Write-Output "[$env:COMPUTERNAME] Acceptable BIOS version should at least be $($AcceptedBIOS)"
+            Exit
+        }
+    }
+    else {
+        Write-Output "[$env:COMPUTERNAME] Model: $($Make.Model)"
+        Write-Output "[$env:COMPUTERNAME] BIOS: $($BIOSver.SMBIOSBIOSVersion)"
+        Write-Output "[$env:COMPUTERNAME] A valid BIOS version was found."
+    }
+}
+
+#
+# If BIOS version is OK, check for required parameter values and update if needed.
+#
+Write-Output ("-" * 80)
+Write-Output "[$env:COMPUTERNAME] Importing DellBIOSProvider module."
+
+# Set-Location "C:\Program Files\WindowsPowerShell\Modules\DellBIOSProvider\2.0.0\"
+# Import-Module '.\DellBIOSProvider.psd1' -Verbose
+# Import-Module '.\DellBIOSProvider.psm1' -Verbose
+Import-Module DellBIOSProvider -Verbose -ErrorAction Stop
+
+Write-Output ("-" * 80)
+Write-Output "[$env:COMPUTERNAME] Dell BIOS System information."
+$Info = Get-ChildItem -Path DellSmbios:\SystemInformation -ErrorAction Stop
+ForEach ($Attrib in $Info) {
+    Write-Output "[$env:COMPUTERNAME] $($Attrib.Attribute): $($Attrib.CurrentValue)"
+}
+
+Write-Output ("-" * 80)
+Write-Output "[$env:COMPUTERNAME] Dell BIOS Powermanagement information."
+$Info = Get-ChildItem -Path DellSmbios:\PowerManagement -ErrorAction Stop
+ForEach ($Attrib in $Info) {
+    Write-Output "[$env:COMPUTERNAME] $($Attrib.Attribute): $($Attrib.CurrentValue)"
+}
+
+Write-Output ("-" * 80)
 $changed = $false
+$password = 'kleinevogel'
 $DeepSleepCtrl = Get-ChildItem -Path DellSmbios:\PowerManagement\DeepSleepCtrl -ErrorAction SilentlyContinue
-$val = $DeepSleepCtrl.CurrentValue
-if($val) {
-    if($val -eq 'Disabled') {        
-    } else {
-        Set-Item -path DellSmbios:\PowerManagement\DeepSleepCtrl -value 'Disabled' -Password $password
+$reqval = 'Disabled'
+if ($DeepSleepCtrl) {
+    $val = $DeepSleepCtrl.CurrentValue
+    if ($val -ne $reqval) {
+        Set-Item -path DellSmbios:\PowerManagement\DeepSleepCtrl -value $reqval -Password $password
+        Write-Output "[$env:COMPUTERNAME] The DeepSleepCtrl parameter was changed to '$reqval'."
         $changed = $true
     }
-    $DeepSleepCtrl = Get-ChildItem -Path DellSmbios:\PowerManagement\DeepSleepCtrl -ErrorAction SilentlyContinue
-    $val = $DeepSleepCtrl.CurrentValue
-    if($changed) { 
-        Write-Output "The Deep Sleep Control feature was changed." 
-    } else {
-        Write-Output "The Deep Sleep Control feature was not changed." 
+    if ($changed) {
+        $WakeOnLan = Get-ChildItem -Path DellSmbios:\PowerManagement\DeepSleepCtrl  -ErrorAction SilentlyContinue
+        $val = $WakeOnLan.CurrentValue
+        if ($val -eq $reqval) {
+            Write-Output "[$env:COMPUTERNAME] SUCCESS: The DeepSleepCtrl parameter was updated successfully."
+        }
+        else {
+            Write-Output "[$env:COMPUTERNAME] ERROR: The DeepSleepCtrl parameter failed to be updated to '$reqval'."
+        }
     }
-    Write-Output "Deep Sleep Control is set to: $val"
+    else {
+        Write-Output "[$env:COMPUTERNAME] SUCCESS: No update needed to the DeepSleepCtrl parameter."
+    }
 }
 else {
-    Write-Host "WARNING: The Deep Sleep Control feature is not supported."
+    Write-Output "[$env:COMPUTERNAME] WARNING: The DeepSleepCtrl parameter is not supported."
 }
 
 $changed = $false
-$WakeOnLan = Get-ChildItem -Path DellSmbios:\PowerManagement\WakeOnLan  -ErrorAction SilentlyContinue
-$val = $WakeOnLan.CurrentValue
-if($val) {
-    if($val -eq 'WLanOnly') {
-    } else {
-        Set-Item -path DellSmbios:\PowerManagement\WakeOnLan -value 'LanOnly' -Password $password
+$WakeOnLan = Get-ChildItem -Path DellSmbios:\PowerManagement\WakeOnLan -ErrorAction SilentlyContinue
+$reqval = 'LanOnly'
+if ($WakeOnLan) {
+    $val = $WakeOnLan.CurrentValue
+    if ($val -ne $reqval) {
+        Set-Item -path DellSmbios:\PowerManagement\WakeOnLan -value $reqval -Password $password
+        Write-Output "[$env:COMPUTERNAME] The WakeOnLan parameter was changed to '$reqval'."
         $changed = $true
     }
-    $WakeOnLan = Get-ChildItem -Path DellSmbios:\PowerManagement\WakeOnLan  -ErrorAction SilentlyContinue
-    $val = $WakeOnLan.CurrentValue
-    if($changed) { 
-        Write-Output "The wake on LAN feature was changed." 
-    } else {
-        Write-Output "The wake on LAN feature was not changed." 
+    if ($changed) {
+        $WakeOnLan = Get-ChildItem -Path DellSmbios:\PowerManagement\WakeOnLan  -ErrorAction SilentlyContinue
+        $val = $WakeOnLan.CurrentValue
+        if ($val -eq $reqval) {
+            Write-Output "[$env:COMPUTERNAME] SUCCESS: The WakeOnLan parameter was updated successfully."
+        }
+        else {
+            Write-Output "[$env:COMPUTERNAME] ERROR: The WakeOnLan parameter failed to be updated to '$reqval'."
+        }
     }
-    Write-Output "Wake on LAN feature is set to: $val"
+    else {
+        Write-Output "[$env:COMPUTERNAME] SUCCESS: No update needed to the WakeOnLan parameter."
+    }
 }
 else {
-    Write-Host "WARNING: The Wake On LAN feature is not supported."
+    Write-Output "[$env:COMPUTERNAME] WARNING: The WakeOnLan parameter is not supported."
 }
+
+Write-Output "[$env:COMPUTERNAME] End of script. Bye bye."
+Write-Output ("-" * 80)
+
+# Stop-Transcript
